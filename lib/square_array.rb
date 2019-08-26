ad446f9d20701475a95fc7cefec290a524f5b725
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-26 10:55:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   counter = 0
   while array[counter]
-    return array[counter] ** array[counter]
+    array[counter] ** array[counter]
   counter +=1
   end
 end
